62ebf45d28cb0bf0d5b648e0ab66a172668b38ee
HoodedUnicorn <68126304+HoodedUnicorn@users.noreply.github.com>
2023-01-30 02:37:47 +0100

    fixed bugs and updated py script

--- FPS-Monitor.py
+++ FPS-Monitor.py
@@ -1,86 +1,61 @@
 import pygame
 import sys
 import psutil
-import gpustat
-import cpuinfo
-
 
 pygame.init()
-height = 700
-width = 300
-screen = pygame.display.set_mode((height, width))
+screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("Century Gothic", 16)
 
+# Create a transparent surface to display the info on
+info_surface = pygame.Surface((200, 200), pygame.SRCALPHA)
 
-def fpsrun():
-    fps = str(int(clock.get_fps()))
-    fps_text = font.render("FPS: " + fps, 1, pygame.Color("white"))
-    return fps_text
-
-
-def get_cpu_usage():
-    cpu_info = cpuinfo.get_cpu_info()['brand_raw']
-    cpu_usage = psutil.cpu_percent()
-
-    if 'AMD' in cpu_info:
-        color = pygame.Color("red")
-    elif 'Intel' in cpu_info:
-        color = pygame.Color("blue")
-    else:
-        color = pygame.Color("white")
-
-    cpu_usage_text = font.render("CPU Usage: " + str(cpu_usage) + "%", 1, color)
-    return cpu_usage_text
-
-def get_cpu_name():
-    cpu_name = cpuinfo.get_cpu_info()['brand_raw']
-    if 'AMD' in cpu_name:
-        cpu_name_text = font.render("CPU Name: " + cpu_name, 1, pygame.Color("red"))
-    elif 'Intel' in cpu_name:
-        cpu_name_text = font.render("CPU Name: " + cpu_name, 1, pygame.Color("blue"))
-    else:
-        cpu_name_text = font.render("CPU Name: " + cpu_name, 1, pygame.Color("white"))
-    return cpu_name_text
-
-
-def get_gpu_usage():
-    gpu_stats = gpustat.GPUStatCollection.new_query()
-    gpu_usage = gpu_stats.gpus[0].utilization
-    gpu_usage_text = font.render("GPU Usage: " + str(gpu_usage) + "%", 1, pygame.Color("green"))
-    return gpu_usage_text
-
-
-def get_gpu_name():
-    gpu_stats = gpustat.GPUStatCollection.new_query()
-    gpu_name = gpu_stats.gpus[0].name
-    gpu_name_text = font.render("GPU Name: " + gpu_name, 1, pygame.Color("green"))
-    return gpu_name_text
-
-
-def get_ram_info():
-    ram_info = psutil.virtual_memory()
-    return "Total RAM: " + str(ram_info.total // (1024**2)) + "MB"
-
-
-def get_ram_usage():
-    ram_usage = psutil.virtual_memory().percent
-    return str(ram_usage) + "%"
-
+start_time = pygame.time.get_ticks()
+frame_count = 0
+fps = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             sys.exit()
-    screen.fill(pygame.Color("black"))
-    screen.blit(fpsrun(), (20, 20))
-    screen.blit(get_cpu_name(), (20, 50))
-    screen.blit(get_cpu_usage(), (20, 70))
-    screen.blit(get_gpu_name(), (20, 100))
-    screen.blit(get_gpu_usage(), (20, 120))
-    screen.blit(font.render("RAM Info: " + get_ram_info(), 1, pygame.Color("orange")), (20, 150))
-    screen.blit(font.render("RAM Usage: " + get_ram_usage(), 1, pygame.Color("orange")), (20, 170))
 
+    screen.fill((0, 0, 0))
+    info_surface.fill((0, 0, 0, 0))  # Clear the surface to make it transparent
+
+    # Display CPU usage
+    cpu_usage = psutil.cpu_percent()
+    cpu_text = font.render("CPU: {:.2f}%".format(cpu_usage), 1, (255, 255, 255))
+    info_surface.blit(cpu_text, (20, 20))
+
+    # Display GPU usage
+    try:
+        import GPUtil
+        gpu_devices = GPUtil.getGPUs()
+        gpu_usage = gpu_devices[0].load * 100
+        gpu_text = font.render("GPU: {:.2f}%".format(gpu_usage), 1, (255, 255, 255))
+        info_surface.blit(gpu_text, (20, 50))
+    except ImportError:
+        pass
+
+    # Display RAM usage
+    ram_usage = psutil.virtual_memory().percent
+    ram_text = font.render("RAM: {:.2f}%".format(ram_usage), 1, (255, 255, 255))
+    info_surface.blit(ram_text, (20, 80))
+
+    # Display FPS
+    current_time = pygame.time.get_ticks()
+    elapsed_time = current_time - start_time
+    frame_count += 1
+
+    if elapsed_time > 1000:
+        fps = frame_count / (elapsed_time / 1000.0)
+        start_time = current_time
+        frame_count = 0
+
+    fps_text = font.render("FPS: {:.2f}".format(fps), 1, (255, 255, 255))
+    info_surface.blit(fps_text, (20, 110))
+
+    # Blit the info surface onto the main screen
+    screen.blit(info_surface, (20, 20))
     pygame.display.update()
     clock.tick(60)
